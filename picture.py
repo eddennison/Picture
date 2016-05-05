41f34990e1b9ae491c960d881ab5f1c3883b8319
eddennison <eddennison@users.noreply.github.com>
2016-05-04 20:16:50 -0400

    Updated from Brython Server: 5/4/2016 8:16:49 PM

--- picture.py
+++ picture.py
@@ -1,7 +1,7 @@
 """
 picture.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Ed Dennison
+Credit: 
 
 Assignment:
 
@@ -27,7 +27,19 @@ for detailed information on ggame.
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # add your code here \/  \/  \/
+# Three primary colors with no transparency (alpha = 1.0)
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
 
+# Define a line style that is a thin (1 pixel) wide black line
+thinline = LineStyle(1, black)
+# A graphics asset that represents a rectangle
+rectangle = RectangleAsset(50, 20, thinline, blue)
+
+# Now display a rectangle
+Sprite(rectangle)
 
 # add your code here /\  /\  /\
 
